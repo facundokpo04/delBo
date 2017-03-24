71a8ee9c812ee1ebf6aee764d98ef9f2b59c0dfa
facundokpo04 <facundokpo04@gmail.com>
2017-03-24 11:40:21 -0300

    Merge origin/master
    Conflicts:
    	www/js/controllers.js

---
+++ application/libraries/Response.php
@@ -0,0 +1,20 @@
+<?php
+
+class Response
+{
+	public $result     = null;
+	public $response   = false;
+	public $message    = 'Ocurrio un error inesperado.';
+        public $errors     = [];
+	
+	public function SetResponse($response, $m='',$result=-1)
+	{
+		$this->response = $response;
+		$this->message = $m;
+                $this->result = $result;
+
+		if(!$response && $m = '') $this->response = 'Ocurrio un error inesperado';
+        
+        return $this;
+	}
+}
