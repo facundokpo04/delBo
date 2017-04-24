a85ef3e78bf120ab54075a3e4689c4fe6879be77
facundo <facundokpo04@gmail.com>
2017-04-24 18:36:32 -0300

    fecha

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -262,7 +262,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="<?php echo base_url(); ?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image" id="emplimg">
-              <span class="hidden-xs">Jorge Jara</span>
+              <span class="hidden-xs">Facundo Dominguez</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
