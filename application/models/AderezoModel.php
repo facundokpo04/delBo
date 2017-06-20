bd64593f026e808bc65c15ddf5002d9f4355616c
facundokpo04 <facundokpo04@gmail.com>
2017-06-20 18:56:12 -0300

    Merge origin/master
    Conflicts:
    	www/js/controllers.js

---
+++ application/models/AderezoModel.php
@@ -0,0 +1,41 @@
+<?php
+class AderezoModel extends CI_Model{
+    
+    public function getAll(){
+        return RestApi::call(
+            RestApiMethod::GET,
+            "aderezo/listar1"
+        );
+    }
+
+    public function obtener($id){
+        return RestApi::call(
+            RestApiMethod::GET,
+            "aderezo/obtener/$id"
+        );
+    }
+    
+    public function registrar($data){
+        
+      
+        return RestApi::call(
+            RestApiMethod::POST,
+            'aderezo/insertar',
+            $data
+        );
+    }
+
+    public function actualizar($data, $id){
+        return RestApi::call(
+            RestApiMethod::PUT,
+            "aderezo/actualizar/$id" , $data
+        );
+    }
+
+     public function eliminar($id){
+        return RestApi::call(
+            RestApiMethod::DELETE,
+            "aderezo/eliminar/$id"
+        );
+    }
+}
