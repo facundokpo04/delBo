d83768c75ab66022d5c1d89652733b87cd65090c
facundo.dominguez <facundokpo04@gmail.com>
2020-06-27 09:37:52 -0300

    cambios categoria js

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -1,8 +1,8 @@
-
-
-
 $('#tblCategorias').DataTable({
-    "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "Todo"]],
+    "lengthMenu": [
+        [5, 10, 15, -1],
+        [5, 10, 15, "Todo"]
+    ],
     'paging': true,
     'info': true,
     'filter': true,
@@ -11,32 +11,33 @@ $('#tblCategorias').DataTable({
         "url": baseurl + "index.php/categoria/get_categorias",
         "type": "POST",
         "dataType": 'json',
-        "data": {'<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>'},
+        "data": { '<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>' },
 
     },
     'columns': [
 
-        {data: 'cat_id', 'sClass': 'dt-body-center'},
-        {data: 'cat_nombre'},
-        {data: 'cat_descripcion'},
-        {data: 'cat_idEstado'},
-        {data: 'cat_idEstadoVisible'},
-        {"orderable": true,
-            render: function (data, type, row) {
+        { data: 'cat_id', 'sClass': 'dt-body-center' },
+        { data: 'cat_nombre' },
+        { data: 'cat_descripcion' },
+        { data: 'cat_idEstado' },
+        { data: 'cat_idEstadoVisible' },
+        {
+            "orderable": true,
+            render: function(data, type, row) {
 
                 return '<span class="pull-right" >' +
-                        '<div class="dropdown">' +
-                        '  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
-                        '    Acciones' +
-                        '  <span class="caret"></span>' +
-                        '  </button>' +
-                        '    <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">' +
-                        '    <li><a href="#" title="Editar informacion" data-toggle="modal" data-target="#modalEditCategoria" onClick="selCategoria(\'' + row.cat_id + '\');"><i style="color:#555;" class="glyphicon glyphicon-edit"></i> Editar</a></li>' +
-                        '    <li><a href="#"><i class="glyphicon glyphicon-eye-open" style="color:#006699"></i> Ver</a></li>' +
-                        '    <li><a href="#" title="Eliminar Categoria" onClick="eliminarCategoria(\'' + row.cat_id + '\');"><i style="color:red;" class="glyphicon glyphicon-remove"></i> Eliminar</a></li>' +
-                        '    </ul>' +
-                        '</div>' +
-                        '</span>';
+                    '<div class="dropdown">' +
+                    '  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
+                    '    Acciones' +
+                    '  <span class="caret"></span>' +
+                    '  </button>' +
+                    '    <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">' +
+                    '    <li><a href="#" title="Editar informacion" data-toggle="modal" data-target="#modalEditCategoria" onClick="selCategoria(\'' + row.cat_id + '\');"><i style="color:#555;" class="glyphicon glyphicon-edit"></i> Editar</a></li>' +
+                    '    <li><a href="#"><i class="glyphicon glyphicon-eye-open" style="color:#006699"></i> Ver</a></li>' +
+                    '    <li><a href="#" title="Eliminar Categoria" onClick="eliminarCategoria(\'' + row.cat_id + '\');"><i style="color:red;" class="glyphicon glyphicon-remove"></i> Eliminar</a></li>' +
+                    '    </ul>' +
+                    '</div>' +
+                    '</span>';
                 // '<a href="#" class="btn btn-block btn-primary btn-sm" style="width: 80%;" data-toggle="modal" data-target="#modalEditCategoria" onClick="selCategoria(\'' + row.cat_id + '\');"><i class="fa fa-fw fa-edit"></i></a></td>';
 
 
@@ -45,11 +46,10 @@ $('#tblCategorias').DataTable({
         }
 
     ],
-    "columnDefs": [
-        {
+    "columnDefs": [{
             "targets": [3],
             "data": "cat_idEstado",
-            "render": function (data, type, row) {
+            "render": function(data, type, row) {
 
                 if (data == 1) {
                     return "<span class='label label-success'>Habilitado</span>";
@@ -62,7 +62,7 @@ $('#tblCategorias').DataTable({
         {
             "targets": [4],
             "data": "cat_idEstadoVisible",
-            "render": function (data, type, row) {
+            "render": function(data, type, row) {
 
                 if (data == 1) {
                     return "<span class='label label-success'>Habilitado</span>";
@@ -76,49 +76,52 @@ $('#tblCategorias').DataTable({
             "targets": [1],
             "data": "cat_nombre",
             "orderData": [1, 0],
-            "render": function (data, type, row) {
+            "render": function(data, type, row) {
                 return "<span style='color:#006699;'><i class='fa fa-cutlery'></i>&nbsp;&nbsp;" + data + "</span>"
 
             }
         },
     ],
-    "order": [[0, "asc"]],
+    "order": [
+        [0, "asc"]
+    ],
 });
 
 
-selCategoria = function (idCategorias) {
+selCategoria = function(idCategorias) {
 
 
     $.ajax({
         type: "POST",
         url: baseurl + "index.php/categoria/get_categoriaById/" + idCategorias,
         dataType: 'json',
-        data: {'<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>'},
-        success: function (res) {
+        data: { '<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>' },
+        success: function(res) {
 
             if (res.estado) {
                 debugger;
-                $('#mDescripcion').val(res.response.cat_descripcion);
-                $('#mNombre').val(res.response.cat_nombre);
-                $('#mEstado').val(res.response.cat_idEstado);
-                $('#mEstadoVisible').val(res.response.cat_idEstadoVisible);
-                $('#imgCategoria').val(res.response.cat_imagen),
-                        //select
-                        //ajax para traer todos los estados
-                        $('#imagen').attr('src', './assets/imagenes/categoria/' + res.response.cat_imagen);
-                $('#mIdCategoria').val(res.response.cat_id);
+                $('#mDescripcion').val(res.response.data.cat_descripcion);
+                $('#mNombre').val(res.response.data.cat_nombre);
+                $('#mEstado').val(res.response.data.cat_idEstado);
+                $('#mEstadoVisible').val(res.response.data.cat_idEstadoVisible);
+                $('#imgCategoria').val(res.response.data.cat_imagen),
+                    //select
+                    //ajax para traer todos los estados
+                    $('#imagen').attr('src', './assets/imagenes/categoria/' + res.response.data.cat_imagen);
+                $('#mIdCategoria').val(res.response.data.cat_id);
             } else {
                 sweetAlert("Oops...", res.response, "error");
                 console.log(res.response);
             }
         },
-        error: function (request, status, error) {
+        error: function(request, status, error) {
             sweetAlert("Oops...", "Ocurrio un Error Inesperado!", "error");
             console.log(error);
         }
     });
 
 };
+
 function guardarImagen() {
     var inputFile = $('input#cImagen');
     var fileToUpload = inputFile[0].files[0];
@@ -139,7 +142,7 @@ function guardarImagen() {
             data: formData,
             processData: false,
             contentType: false,
-            success: function (res) {
+            success: function(res) {
 
                 if (res.estado) {
 
@@ -148,7 +151,7 @@ function guardarImagen() {
                         text: "No se olvide de Actualizar para Guardar los Cambios",
                         type: "success",
                         showLoaderOnConfirm: true,
-                    }, function () {
+                    }, function() {
 
                         $('#imagen').attr('src', './assets/imagenes/categoria/' + res.response);
                         $('#imgCategoria').val(res.response);
@@ -161,7 +164,7 @@ function guardarImagen() {
 
                 }
             },
-            error: function (request, status, error) {
+            error: function(request, status, error) {
                 sweetAlert("Oops...", "Ocurrio un Error Inesperado!", "error");
                 console.log(error.message);
 
@@ -170,79 +173,78 @@ function guardarImagen() {
     } else {
         sweetAlert("Oops...", "Seleccione un Imagen", "warning");
     }
-}
-;
+};
 
 function eliminarCategoria(idCategoria) {
     swal({
-        title: "Esta seguro?",
-        text: "Se eliminar la Categoria",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Si, Eliminar!",
-        cancelButtonText: "No, Cancelar!",
-        closeOnConfirm: false,
-        closeOnCancel: false
-    },
-            function (isConfirm) {
-                if (isConfirm) {
-                    $.ajax({
-                        type: "POST",
-                        url: baseurl + "index.php/categoria/eliminar/" + idCategoria,
-                        dataType: 'json',
-                        data: {'<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>'},
+            title: "Esta seguro?",
+            text: "Se eliminar la Categoria",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Si, Eliminar!",
+            cancelButtonText: "No, Cancelar!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
+        function(isConfirm) {
+            if (isConfirm) {
+                $.ajax({
+                    type: "POST",
+                    url: baseurl + "index.php/categoria/eliminar/" + idCategoria,
+                    dataType: 'json',
+                    data: { '<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>' },
 
-                        success: function (res) {
+                    success: function(res) {
 
 
-                            if (res.estado) {
+                        if (res.estado) {
 
-                                swal({
+                            swal({
                                     title: "Eliminado",
                                     text: "La Categoria se a eliminado!",
                                     type: "success",
                                 },
-                                        function () {
-                                            location.reload();
-                                        });
+                                function() {
+                                    location.reload();
+                                });
 
-                            } else {
-                                sweetAlert("Ocurrio un Error", res.response, "error");
-
-                            }
-
-                        },
-                        error: function (request, status, error) {
-
-                            console.log(error);
-                            sweetAlert("Ocurrio un Error Inesperado", error, "error");
+                        } else {
+                            sweetAlert("Ocurrio un Error", res.response, "error");
 
                         }
-                    });
-                } else {
-                    swal("Cancelado", "La Categoria no fue Eliminada", "error");
-                }
-            });
+
+                    },
+                    error: function(request, status, error) {
+
+                        console.log(error);
+                        sweetAlert("Ocurrio un Error Inesperado", error, "error");
+
+                    }
+                });
+            } else {
+                swal("Cancelado", "La Categoria no fue Eliminada", "error");
+            }
+        });
 
 }
 
 
-$('#mbtnCerrarModal').click(function () {
+$('#mbtnCerrarModal').click(function() {
 
     $('#mDescripcion').val('');
     $('#mNombre').val('');
-    $('#mEstado').val('1');//select
+    $('#mEstado').val('1'); //select
     $('#mEstadoVisible').val('1');
     //ajax para traer todos los estados
     $('#mIdCategoria').val('');
     $('#imgCategoria').val('');
     $('.nav-tabs a:first').tab('show')
 })
-$('#mCerrarModal').click(function () {
+$('#mCerrarModal').click(function() {
     $('#mDescripcion').val('');
     $('#mNombre').val('');
-    $('#mEstado').val('1');//select
+    $('#mEstado').val('1'); //select
     $('#mEstadoVisible').val('1');
     //ajax para traer todos los estados
     $('#mIdCategoria').val('');
@@ -251,12 +253,13 @@ $('#mCerrarModal').click(function () {
 
 })
 
-$('#mbtnUpdCategoria').click(function () {
+$('#mbtnUpdCategoria').click(function() {
     $.ajax({
         type: "POST",
         url: baseurl + "index.php/categoria/updCategoria",
         dataType: 'json',
-        data: {'<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>',
+        data: {
+            '<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>',
             cat_nombre: $('#mNombre').val(),
             cat_descripcion: $('#mDescripcion').val(),
             cat_idEstado: $('#mEstado').val(),
@@ -264,24 +267,24 @@ $('#mbtnUpdCategoria').click(function () {
             cat_imagen: $('#imgCategoria').val(),
             cat_id: $('#mIdCategoria').val()
         },
-        success: function (res) {
+        success: function(res) {
             debugger;
             if (res.estado) {
                 swal({
-                    title: "Los Datos Fueron Guardados!",
-                    text: "haga click!",
-                    type: "success",
-                },
-                        function () {
-                            $('#mbtnCerrarModal').click();
-                            location.reload();
-                        });
+                        title: "Los Datos Fueron Guardados!",
+                        text: "haga click!",
+                        type: "success",
+                    },
+                    function() {
+                        $('#mbtnCerrarModal').click();
+                        location.reload();
+                    });
             } else {
                 sweetAlert("Oops...", JSON.stringify(res.response), "error");
                 console.log(res.response);
             }
         },
-        error: function (request, status, error) {
+        error: function(request, status, error) {
             sweetAlert("Oops...", "Ocurrio un Error Inesperado!", "error");
             console.log(error);
         }
@@ -292,8 +295,7 @@ $('#mbtnUpdCategoria').click(function () {
 
 
 
-$('#btnGuardarImg').click(function () {
+$('#btnGuardarImg').click(function() {
 
     guardarImagen();
 })
-         
